cf9c021d0de4769ca81f9ec7151eef05eb923ae5
SokolovskiyAA <sokolovsky.a.a@yandex.ru>
2022-08-30 22:45:21 +0300

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const { createUser, login } = require('../controllers/users');
 router.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required().min(3),
     name: Joi.string().required().min(2).max(30),
   }),
 }), createUser);
